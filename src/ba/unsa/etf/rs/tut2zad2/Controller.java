d6bc846af6c8037f80c45004cc0ca364dde3eaee
Your Name <sjukic1@etf.unsa.ba>
2020-03-18 01:52:57 +0100

    Dadan izgled

--- src/ba/unsa/etf/rs/tut2zad2/Controller.java
+++ src/ba/unsa/etf/rs/tut2zad2/Controller.java
@@ -1,4 +1,27 @@
 package ba.unsa.etf.rs.tut2zad2;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public TextField unosBroja;
+    public TextArea ispisBrojeva;
+
+
+    public void sumaCifara(ActionEvent actionEvent) {
+        int n = Integer.parseInt(unosBroja.getText());
+        String izlazni = new String();
+        for(int i=1; i<=n; i++){
+            int j = i, suma=0;
+            while(j!=0){
+                suma = suma + (j%10);
+                j=j/10;
+            }
+            if(i%suma == 0){
+                izlazni = izlazni + Integer.toString(i) + " ";
+            }
+        }
+        ispisBrojeva.setText(izlazni);
+    }
 }
